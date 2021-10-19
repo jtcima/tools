2420b1abc0d7e7640c79adfef02d4b8988ba120a
jt <jt@JT-Station.jtcima.com>
2021-10-19 11:30:05 +1100

    1810

--- startup.h
+++ startup.h
@@ -1,9 +1,6 @@
 #ifndef STARTUP_H
 #define STARTUP_H
 
-#define HEIGHT  20
-#define WIDTH   80
-
 void startup(void);
 
 #endif
--- tools.c
+++ tools.c
@@ -6,7 +6,7 @@
 int main(int argc, char** argv)
 {
     
-    //startup();
+    startup();
     caesar_decrypt();
     return 0;
 }
